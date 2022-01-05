6aabaa3bf9a055e10ce3b236d9edde3fd6ecbda2
Hrishikesh-Bhagwat <bhagwatcoder@gmail.com>
2022-01-05 19:42:08 +0530

    Edit

--- app.js
+++ app.js
@@ -5,24 +5,146 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.json());
 
 const cashDelivery = [
-  "400001", "400002", "400003", "400004", "400005", "400006", "400007", "400008", "400009",
-  "400010", "400011", "400012", "400013", "400014", "400015", "400016", "400017", "400018",
-  "400019", "400020", "400021", "400022", "400024", "400025", "400026", "400027", "400028",
-  "400029", "400030", "400031", "400032", "400033", "400034", "400035", "400037", "400043",
-  "400049", "400050", "400051", "400052", "400053", "400054", "400055", "400056", "400057",
-  "400058", "400059", "400063", "400066", "400067", "400069", "400070", "400071", "400074",
-  "400075", "400077", "400080", "400084", "400085", "400086", "400088", "400089", "400093",
-  "400094", "400098", "400099", "400101", "400102", "400104",
+  "400001",
+  "400002",
+  "400003",
+  "400004",
+  "400005",
+  "400006",
+  "400007",
+  "400008",
+  "400009",
+  "400010",
+  "400011",
+  "400012",
+  "400013",
+  "400014",
+  "400015",
+  "400016",
+  "400017",
+  "400018",
+  "400019",
+  "400020",
+  "400021",
+  "400022",
+  "400024",
+  "400025",
+  "400026",
+  "400027",
+  "400028",
+  "400029",
+  "400030",
+  "400031",
+  "400032",
+  "400033",
+  "400034",
+  "400035",
+  "400037",
+  "400043",
+  "400049",
+  "400050",
+  "400051",
+  "400052",
+  "400053",
+  "400054",
+  "400055",
+  "400056",
+  "400057",
+  "400058",
+  "400059",
+  "400063",
+  "400066",
+  "400067",
+  "400069",
+  "400070",
+  "400071",
+  "400074",
+  "400075",
+  "400077",
+  "400080",
+  "400084",
+  "400085",
+  "400086",
+  "400088",
+  "400089",
+  "400093",
+  "400094",
+  "400098",
+  "400099",
+  "400101",
+  "400102",
+  "400104",
 ];
 const mainDelivery = [
-  "400001", "400002", "400003", "400004", "400005", "400006", "400007", "400008", "400009",
-  "400010", "400011", "400012", "400013", "400014", "400015", "400016", "400017", "400018",
-  "400019", "400020", "400021", "400022", "400024", "400025", "400026", "400027", "400028",
-  "400029", "400030", "400031", "400032", "400033", "400034", "400035", "400037", "400043",
-  "400049", "400050", "400051", "400052", "400053", "400054", "400055", "400056", "400057",
-  "400058", "400059", "400063", "400066", "400067", "400069", "400070", "400071", "400074",
-  "400075", "400077", "400080", "400084", "400085", "400086", "400088", "400089", "400093",
-  "400094", "400098", "400099", "400101", "400102", "400104",
+  "400001",
+  "400002",
+  "400003",
+  "400004",
+  "400005",
+  "400006",
+  "400007",
+  "400008",
+  "400009",
+  "400010",
+  "400011",
+  "400012",
+  "400013",
+  "400014",
+  "400015",
+  "400016",
+  "400017",
+  "400018",
+  "400019",
+  "400020",
+  "400021",
+  "400022",
+  "400024",
+  "400025",
+  "400026",
+  "400027",
+  "400028",
+  "400029",
+  "400030",
+  "400031",
+  "400032",
+  "400033",
+  "400034",
+  "400035",
+  "400037",
+  "400043",
+  "400049",
+  "400050",
+  "400051",
+  "400052",
+  "400053",
+  "400054",
+  "400055",
+  "400056",
+  "400057",
+  "400058",
+  "400059",
+  "400063",
+  "400066",
+  "400067",
+  "400069",
+  "400070",
+  "400071",
+  "400074",
+  "400075",
+  "400077",
+  "400080",
+  "400084",
+  "400085",
+  "400086",
+  "400088",
+  "400089",
+  "400093",
+  "400094",
+  "400098",
+  "400099",
+  "400101",
+  "400102",
+  "400104",
 ];
 
 app.post("/profile-handler", (req, res) => {
@@ -114,87 +236,164 @@ app.post("/add-game-quote", async (req, res) => {
       },
       body: JSON.stringify({ op: "one", find: { uid: userId } }),
     }
-  ).then(async function (r) {
-    if(r.status!=200){
-      throw r.status.toString();
-    }
-    var detailBody = await r.json();
-    if (method == "sell" && detailBody.result.is_sell == false) {
+  )
+    .then(async function (r) {
+      if (r.status != 200) {
+        throw r.status.toString();
+      }
+      var detailBody = await r.json();
+      if (method == "sell" && detailBody.result.is_sell == false) {
+        return res.send(
+          JSON.stringify({
+            status: "error",
+            message: "Selling not supported from your location yet",
+          })
+        );
+      } else {
+        var addQuoteBody = JSON.stringify({
+          op: "one",
+          doc: {
+            game_id: gameId,
+            user_id: userId,
+            price: price,
+            quantity: quantity,
+            name: detailBody.result.name,
+            method: method,
+            line1: detailBody.result.line1,
+            line2: detailBody.result.line2,
+            line3: detailBody.result.line3,
+            pin: detailBody.result.pin,
+            phone: detailBody.result.phone,
+            is_cash: detailBody.result.is_cash,
+            is_sell: detailBody.result.is_sell,
+            email: detailBody.result.email,
+          },
+        });
+        var quoteResponse = await fetch(
+          "https://tradebooksapp.com/v1/api/tradebooks/crud/postgres/game_quotes/create",
+          {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+            body: addQuoteBody,
+          }
+        );
+        if (quoteResponse.status != 200) {
+          console.log(await quoteResponse.json());
+          throw "Error in adding quote";
+        } else {
+          var event = await fetch(
+            "https://tradebooksapp.com/v1/api/tradebooks/eventing/queue",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                type: "game_quote",
+                payload: { game_id: gameId },
+              }),
+            }
+          );
+          if (event.status != 200) {
+            return res.send(
+              JSON.stringify({
+                status: "error",
+                message: "Error in event",
+              })
+            );
+          }
+          return res.send(
+            JSON.stringify({
+              status: "success",
+              message: "Successfully added quote",
+            })
+          );
+        }
+      }
+    })
+    .catch((e) => {
       return res.send(
         JSON.stringify({
           status: "error",
-          message: "Selling not supported from your location yet",
+          message: `An error occurred ${e.toString()}`,
         })
       );
-    } else {
-      var addQuoteBody = JSON.stringify({
-        op: "one",
-        doc: {
-          game_id: gameId,
-          user_id: userId,
-          price: price,
-          quantity: quantity,
-          name: detailBody.result.name,
-          method: method,
-          line1: detailBody.result.line1,
-          line2: detailBody.result.line2,
-          line3: detailBody.result.line3,
-          pin: detailBody.result.pin,
-          phone: detailBody.result.phone,
-          is_cash: detailBody.result.is_cash,
-          is_sell: detailBody.result.is_sell,
-          email: detailBody.result.email,
-        },
-      });
-      var quoteResponse=await fetch("https://tradebooksapp.com/v1/api/tradebooks/crud/postgres/game_quotes/create",{
+    });
+});
+
+app.post("/update-game-quote", async (req, res) => {
+  var body = req.body;
+  var userId = body.user_id;
+  var price = body.price;
+  var quantity = body.quantity;
+  var token = body.token;
+  var quoteId = body.quoteId;
+  var updateQuoteBody = JSON.stringify({
+    op: "one",
+    find: {
+      user_id: userId,
+      id: quoteId,
+    },
+    update: {
+      price: price,
+      quantity: quantity,
+    },
+  });
+  try {
+    var quoteResponse = await fetch(
+      "https://tradebooksapp.com/v1/api/tradebooks/crud/postgres/game_quotes/update",
+      {
         method: "POST",
         headers: {
           Authorization: "Bearer " + token,
           "Content-Type": "application/json",
         },
-        body: addQuoteBody
-      })
-      if(quoteResponse.status!=200){
-        console.log(await quoteResponse.json());
-        throw "Error in adding quote"
-      }else{
-        var event=await fetch("https://tradebooksapp.com/v1/api/tradebooks/eventing/queue",{
-          method:"POST",
-          headers:{
+        body: updateQuoteBody,
+      }
+    );
+    if (quoteResponse.status != 200) {
+      console.log(await quoteResponse.json());
+      throw "Error in adding quote";
+    } else {
+      var event = await fetch(
+        "https://tradebooksapp.com/v1/api/tradebooks/eventing/queue",
+        {
+          method: "POST",
+          headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(
-            {
-              type:"game_quote",payload:{game_id: gameId}
-            }
-          )
-        })
-        if(event.status!=200){
-          return res.send(
-            JSON.stringify({
-              status:"error",
-              message:"Error in event"
-            })
-          )
+          body: JSON.stringify({
+            type: "game_quote",
+            payload: { game_id: gameId },
+          }),
         }
+      );
+      if (event.status != 200) {
         return res.send(
           JSON.stringify({
-            status:"success",
-            message:"Successfully added quote"
+            status: "error",
+            message: "Error in event",
           })
-        )
+        );
       }
+      return res.send(
+        JSON.stringify({
+          status: "success",
+          message: "Successfully added quote",
+        })
+      );
     }
-  }).catch(e=>{
+  } catch (e) {
     return res.send(
-      JSON.stringify(
-        {
-          status:"error",
-          message:`An error occurred ${e.toString()}`
-        }
-      )
-    )
-  });
+      JSON.stringify({
+        status: "error",
+        message: `An error occurred ${e.toString()}`,
+      })
+    );
+  }
 });
 
 app.post("/add-book-quote", async (req, res) => {
@@ -215,87 +414,92 @@ app.post("/add-book-quote", async (req, res) => {
       },
       body: JSON.stringify({ op: "one", find: { uid: userId } }),
     }
-  ).then(async function (r) {
-    if(r.status!=200){
-      throw r.status.toString();
-    }
-    var detailBody = await r.json();
-    if (method == "sell" && detailBody.result.is_sell == false) {
+  )
+    .then(async function (r) {
+      if (r.status != 200) {
+        throw r.status.toString();
+      }
+      var detailBody = await r.json();
+      if (method == "sell" && detailBody.result.is_sell == false) {
+        return res.send(
+          JSON.stringify({
+            status: "error",
+            message: "Selling not supported from your location yet",
+          })
+        );
+      } else {
+        var addQuoteBody = JSON.stringify({
+          op: "one",
+          doc: {
+            book_id: bookId,
+            user_id: userId,
+            price: price,
+            quantity: quantity,
+            name: detailBody.result.name,
+            method: method,
+            line1: detailBody.result.line1,
+            line2: detailBody.result.line2,
+            line3: detailBody.result.line3,
+            pin: detailBody.result.pin,
+            phone: detailBody.result.phone,
+            is_cash: detailBody.result.is_cash,
+            is_sell: detailBody.result.is_sell,
+            email: detailBody.result.email,
+          },
+        });
+        var quoteResponse = await fetch(
+          "https://tradebooksapp.com/v1/api/tradebooks/crud/postgres/book_quotes/create",
+          {
+            method: "POST",
+            headers: {
+              Authorization: "Bearer " + token,
+              "Content-Type": "application/json",
+            },
+            body: addQuoteBody,
+          }
+        );
+        if (quoteResponse.status != 200) {
+          console.log(await quoteResponse.json());
+          throw "Error in adding quote";
+        } else {
+          var event = await fetch(
+            "https://tradebooksapp.com/v1/api/tradebooks/eventing/queue",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                type: "book_quote",
+                payload: { book_id: bookId },
+              }),
+            }
+          );
+          if (event.status != 200) {
+            return res.send(
+              JSON.stringify({
+                status: "error",
+                message: "Error in event",
+              })
+            );
+          }
+          return res.send(
+            JSON.stringify({
+              status: "success",
+              message: "Successfully added quote",
+            })
+          );
+        }
+      }
+    })
+    .catch((e) => {
       return res.send(
         JSON.stringify({
           status: "error",
-          message: "Selling not supported from your location yet",
+          message: `An error occurred ${e.toString()}`,
         })
       );
-    } else {
-      var addQuoteBody = JSON.stringify({
-        op: "one",
-        doc: {
-          book_id: bookId,
-          user_id: userId,
-          price: price,
-          quantity: quantity,
-          name: detailBody.result.name,
-          method: method,
-          line1: detailBody.result.line1,
-          line2: detailBody.result.line2,
-          line3: detailBody.result.line3,
-          pin: detailBody.result.pin,
-          phone: detailBody.result.phone,
-          is_cash: detailBody.result.is_cash,
-          is_sell: detailBody.result.is_sell,
-          email: detailBody.result.email,
-        },
-      });
-      var quoteResponse=await fetch("https://tradebooksapp.com/v1/api/tradebooks/crud/postgres/book_quotes/create",{
-        method: "POST",
-        headers: {
-          Authorization: "Bearer " + token,
-          "Content-Type": "application/json",
-        },
-        body: addQuoteBody
-      })
-      if(quoteResponse.status!=200){
-        console.log(await quoteResponse.json());
-        throw "Error in adding quote"
-      }else{
-        var event=await fetch("https://tradebooksapp.com/v1/api/tradebooks/eventing/queue",{
-          method:"POST",
-          headers:{
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(
-            {
-              type:"book_quote",payload:{book_id: bookId}
-            }
-          )
-        })
-        if(event.status!=200){
-          return res.send(
-            JSON.stringify({
-              status:"error",
-              message:"Error in event"
-            })
-          )
-        }
-        return res.send(
-          JSON.stringify({
-            status:"success",
-            message:"Successfully added quote"
-          })
-        )
-      }
-    }
-  }).catch(e=>{
-    return res.send(
-      JSON.stringify(
-        {
-          status:"error",
-          message:`An error occurred ${e.toString()}`
-        }
-      )
-    )
-  });
+    });
 });
 
 app.post("/handle-book-quote", (req, res) => {
